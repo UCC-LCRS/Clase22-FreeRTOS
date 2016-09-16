ab85a7d52f8b48e4282af74803c822be7552dcb3
Martin Marcucci <rmarku@gmail.com>
2016-09-16 10:33:31 -0300

    se suspenden y resumen tareas

---
+++ src/FreeRTOSConfig.h
@@ -0,0 +1,13 @@
+/*
+ * FreeRTOSConfig.h
+ *
+ *  Created on: 16 de sept. de 2016
+ *      Author: LCSR-AF
+ */
+
+#ifndef FREERTOSCONFIG_H_
+#define FREERTOSCONFIG_H_
+
+#define INCLUDE_vTaskSuspend                    1
+
+#endif /* FREERTOSCONFIG_H_ */
--- src/main.c
+++ src/main.c
@@ -1,65 +1,68 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
 
 #include "bsp.h"
-			
+
 /* Standard includes */
 #include <stdio.h>                          // prototype declarations for I/O functions
-
 
 /* Scheduler includes */
 #include "FreeRTOS.h"
 #include "task.h"
 
-void prvTaskA (void* pvParameters)
-{
-    (void) pvParameters;                    // Just to stop compiler warnings.
+TaskHandle_t tareaB;
 
-    for (;;) {
-    	led_setBright(LED_ROJO,0);
-        vTaskDelay(500);
-    	led_setBright(LED_ROJO,100);
-        vTaskDelay(500);
-    }
+void prvTaskA(void* pvParameters) {
+	(void) pvParameters;                    // Just to stop compiler warnings.
+
+	for (;;) {
+		led_setBright(LED_ROJO, 0);
+		vTaskDelay(500);
+		led_setBright(LED_ROJO, 100);
+		vTaskDelay(500);
+		vTaskResume(tareaB);
+	}
 }
 
-void prvTaskB (void* pvParameters)
-{
-    (void) pvParameters;                    // Just to stop compiler warnings.
-
-    for (;;) {
-    	led_setBright(LED_VERDE,0);
-        vTaskDelay(1000);
-    	led_setBright(LED_VERDE,100);
-        vTaskDelay(1000);
-    }
+void prvTaskB(void* pvParameters) {
+	(void) pvParameters;                    // Just to stop compiler warnings.
+	uint8_t status;
+	for (;;) {
+		if (status) {
+			led_setBright(LED_VERDE, 0);
+			status = 0;
+		} else {
+			led_setBright(LED_VERDE, 100);
+			status = 1;
+		}
+		vTaskSuspend(NULL);
+	}
 }
 
-int main (void)
-{
+int main(void) {
 // Init BSP
-BSP_Init();
+	BSP_Init();
 
-    xTaskCreate( prvTaskA, ( signed char * ) "TaskA", configMINIMAL_STACK_SIZE, NULL,
-        tskIDLE_PRIORITY, ( xTaskHandle * ) NULL );
-    xTaskCreate( prvTaskB, ( signed char * ) "TaskB", configMINIMAL_STACK_SIZE, NULL,
-        tskIDLE_PRIORITY, ( xTaskHandle * ) NULL );
+	xTaskCreate(prvTaskA, (signed char * ) "TaskA", configMINIMAL_STACK_SIZE,
+			NULL, tskIDLE_PRIORITY, ( xTaskHandle * ) NULL);
+	xTaskCreate(prvTaskB, (signed char * ) "TaskB", configMINIMAL_STACK_SIZE,
+			NULL, tskIDLE_PRIORITY,&tareaB);
 
-    vTaskStartScheduler();
+	vTaskStartScheduler();
 
-   //should never get here
-   printf("ERORR: vTaskStartScheduler returned!");
-   for (;;);
+	//should never get here
+	printf("ERORR: vTaskStartScheduler returned!");
+	for (;;)
+		;
 }
 
-void APP_1ms(){
-
+void APP_1ms() {
 
 }
